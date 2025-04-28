874aff212251b21af4d4d4b659709f0e1417fc6f
Leonardo Gasparic <leonardo.gasparic12@gmail.com>
2025-04-28 14:40:06 +0200

    dorada zad 4. 2.dio

--- main4.cpp
+++ main4.cpp
@@ -3,7 +3,6 @@
 #include <cstdlib>
 #include <ctime>
 #include <stdexcept>
-
 using namespace std;
 
 void ispisiNiz(const vector<int>& niz) {
@@ -47,3 +46,67 @@ void selectionSort(vector<int>& niz, bool uzlazno, int& koraci, int& zamjene) {
     }
 }
 
+int main() {
+    try {
+        srand(time(0)); 
+
+        int N;
+        cout << "Unesite broj elemenata niza: ";
+        cin >> N;
+
+        if (N <= 0) {
+            throw invalid_argument("Broj elemenata mora biti pozitivan!");
+        }
+
+        vector<int> niz(N);
+
+        for (int& broj : niz) {
+            broj = rand() % 1000 + 1; 
+        }
+
+        char izborSortiranja;
+        cout << "Sortirati uzlazno - u ili silazno - s? ";
+        cin >> izborSortiranja;
+
+        bool uzlazno;
+        if (izborSortiranja == 'u' || izborSortiranja == 'U') {
+            uzlazno = true;
+        } else if (izborSortiranja == 's' || izborSortiranja == 'S') {
+            uzlazno = false;
+        } else {
+            throw invalid_argument("Neispravan unos za sortiranje!");
+        }
+
+        char izborAlgoritma;
+        cout << "Odaberite algoritam: bubble sort (b) ili selection sort (s): ";
+        cin >> izborAlgoritma;
+
+        if (N < 50) {
+            cout << "Ulazni niz:\n";
+            ispisiNiz(niz);
+        }
+
+        int koraci = 0, zamjene = 0;
+
+        if (izborAlgoritma == 'b' || izborAlgoritma == 'B') {
+            bubbleSort(niz, uzlazno, koraci, zamjene);
+        } else if (izborAlgoritma == 's' || izborAlgoritma == 'S') {
+            selectionSort(niz, uzlazno, koraci, zamjene);
+        } else {
+            throw invalid_argument("Neispravan unos algoritma!");
+        }
+
+        if (N < 50) {
+            cout << "Sortirani niz:\n";
+            ispisiNiz(niz);
+        }
+
+        cout << "Ukupno koraka: " << koraci << endl;
+        cout << "Ukupno zamjena: " << zamjene << endl;
+    }
+    catch (const exception& e) {
+        cerr << "Greska: " << e.what() << endl;
+    }
+
+    return 0;
+}

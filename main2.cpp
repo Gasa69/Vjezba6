3fcd660c0fd27640811325cc6af81a3bf4f63223
Leonardo Gasparic <leonardo.gasparic12@gmail.com>
2025-04-28 11:30:34 +0200

    dorada 2.zad dio 2.

--- main2.cpp
+++ main2.cpp
@@ -73,4 +73,100 @@ void snimiUDatoteku(const vector<int>& brojevi) {
         throw runtime_error("Greška kod otvaranja datoteke za pisanje.");
     }
 
-  
+    datoteka << brojevi.size() << endl;
+    for (int broj : brojevi) {
+        datoteka << broj << endl;
+    }
+
+    cout << "Podaci su uspješno spremljeni." << endl;
+}
+
+void ucitajIzDatoteke(vector<int>& brojevi) {
+    string nazivDatoteke;
+    cout << "Unesite naziv datoteke: ";
+    cin >> nazivDatoteke;
+
+    ifstream datoteka(nazivDatoteke);
+    if (!datoteka.is_open()) {
+        throw runtime_error("Greška kod otvaranja datoteke za čitanje.");
+    }
+
+    int N;
+    datoteka >> N;
+    if (datoteka.fail() || N < 0) {
+        throw runtime_error("Neispravan format datoteke (broj elemenata).");
+    }
+
+    brojevi.clear();
+    int broj;
+    while (datoteka >> broj) {
+        brojevi.push_back(broj);
+    }
+
+    cout << "Podaci su uspješno učitani." << endl;
+}
+
+void ispisSortiranog(const vector<int>& brojevi) {
+    vector<int> sortirani = brojevi;
+    sort(sortirani.begin(), sortirani.end());
+    cout << "Sortirani brojevi: ";
+    for (int broj : sortirani) {
+        cout << broj << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> brojevi;
+    int izbor;
+
+    do {
+        cout << "\nIzbornik:" << endl;
+        cout << "1. Unos brojeva" << endl;
+        cout << "2. Ispis brojeva unazad" << endl;
+        cout << "3. Ispis razlika izmedju brojeva" << endl;
+        cout << "4. Najmanji i najveci broj" << endl;
+        cout << "5. Snimanje u datoteku" << endl;
+        cout << "6. Učitavanje iz datoteke" << endl;
+        cout << "7. Ispis sortiranog popisa" << endl;
+        cout << "8. Izlaz" << endl;
+        cout << "Odabir: ";
+        cin >> izbor;
+
+        try {
+            switch (izbor) {
+                case 1:
+                    unosBrojeva(brojevi);
+                    break;
+                case 2:
+                    ispisObrnuto(brojevi);
+                    break;
+                case 3:
+                    ispisRazlika(brojevi);
+                    break;
+                case 4:
+                    pronadiMinMax(brojevi);
+                    break;
+                case 5:
+                    snimiUDatoteku(brojevi);
+                    break;
+                case 6:
+                    ucitajIzDatoteke(brojevi);
+                    break;
+                case 7:
+                    ispisSortiranog(brojevi);
+                    break;
+                case 8:
+                    cout << "Izlaz iz programa." << endl;
+                    break;
+                default:
+                    cout << "Neispravan unos!" << endl;
+            }
+        } catch (const exception& e) {
+            cout << "Greška: " << e.what() << endl;
+        }
+
+    } while (izbor != 8);
+
+    return 0;
+}
